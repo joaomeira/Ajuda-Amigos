96951563b438e39a4c4d25ed9ada1a5ba4540803
João Meira <29551891+joaomeira@users.noreply.github.com>
2023-09-28 10:00:32 -0300

    Identação, if após os elif, e corrigido os comentarios

--- Gabriel Santos/Exercicio270923-Editado.py
+++ Gabriel Santos/Exercicio270923-Editado.py
@@ -1,19 +1,19 @@
-# vpontosAriáveis de pontos
+# variáveis de pontos
 pontosAgua = 0
 pontosFogo = 0
 pontosTerra = 0
 pontosAr = 0
-#DICA: Sempre utilize nomes em vpontosAriaveis mais especifica.
+#DICA: Sempre utilize nomes em variaveis mais especificos.
 #Melhora leitura do codigo, ajuda na manutenção do mesmo.
 
-# vpontosAriável elemento
+# variável elemento
 elemento = 0
 
 while True:
     # Captura o elemento a ser digitado.
     elemento = str(input())
 
-    # Finaliza (break), caso a captura de cpontosAracter seja
+    # Finaliza (break), caso a captura de caracter seja
     # FIM.... Saindo do while....!!!
     if elemento == 'FIM':
         # print('FIM') # Retirado o print fim, pois no exercicio não pedia
@@ -24,43 +24,48 @@ while True:
     # Caso não seja apenas ignora voltando ao inicio do WHILE
     treino = int(input())
 
-    # Caso seja o elemento pontosAgua, adiciona a pontuação do treino em pontosAgua
-    # Como pontosAgua é inverso de pontosFogo, perdesse metade da habilidade adiqui
-    # rida em pontosFogo.... Mesma logica ppontosAra as demais.
+    # Caso seja o elemento agua, adiciona a pontuação do treino em agua
+    # como agua é inverso de fogo, perdesse metade da habilidade adiqui-
+    # rida em fogo.... Mesma logica para as demais.
     if elemento == 'AGUA':
         pontosAgua = (pontosAgua + treino)
         pontosFogo = (pontosFogo / 2)
-    if pontosFogo < 0:
+
         # Um dos erros cometidos foi na utilização de operador relacional (==)
-        # invés de utilizpontosAr operador de atribuição (=)
-        # Operadores relacionais são comunmente utilizados em comppontosArações
+        # invés de utilizar operador de atribuição (=)
+        # Operadores relacionais são comunmente utilizados em comparações
         # x ==(é igual..?) x .... Retorno true (SIM)
-        pontosFogo = 0
+        if pontosFogo < 0:
+            pontosFogo = 0
+
     elif elemento == 'FOGO':
         pontosFogo = (pontosFogo + treino)
         pontosAgua = (pontosAgua / 2)
-    if pontosAgua < 0:
-        pontosAgua = 0
+        if pontosAgua < 0:
+            pontosAgua = 0
+
     elif elemento == 'TERRA':
-        # Uma opção ppontosAra encurtpontosAr o codigo é também se aproveitpontosAr dos operadores
-        # de atribuição..... Nesse caso é interessane utilizpontosAr o += atribuindo
+        # Uma opção para encurtar o codigo é também se aproveitar dos operadores
+        # de atribuição..... Nesse caso é interessane utilizar o += atribuindo
         # x += y é equivalente a x = x + y
         # Observe como foi utilizado no elemento abaixo....
         pontosTerra += treino
         pontosAr /= 2
-    if pontosAr < 0:
-        pontosAr = 0
+
+        if pontosAr < 0:
+            pontosAr = 0
+
     elif elemento == 'AR':
         # Outro ponto de erro foram as identações..... Ficar atento pois
         # o python as utiliza para identificar onde será feita a operação..!!
         pontosAr = (pontosAr + treino)
         pontosTerra = (pontosTerra / 2)
-    if pontosTerra < 0:
-        pontosTerra = 0
+        if pontosTerra < 0:
+            pontosTerra = 0
 
 # Imprime na tela o resultado do treinamento, ao final, caso todos elementos
 # sejam maior que 0 também é informado que o treinamento foi um sucesso
-# do contrpontosArio informa ppontosAra realizpontosAr mais treinamentos.
+# do contrario informa para realizar mais treinamentos.
 print('Pontuacao Final')
 print(f'Agua : {pontosAgua:.1f}')
 print(f'Terra : {pontosTerra:.1f}')
